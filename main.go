6f300239f05434ed4094954cc557c9f1d87f1259
Tien Thanh <tthanh.v9.16@gmail.com>
2016-06-28 15:43:17 +0700

    basic check duplicate file logic

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"flag"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 )
 
 var (
-	cPath   = "/run/media/tthanh/media/images"
 	allFile map[int64][]*File
 )
 
@@ -16,9 +21,19 @@ type File struct {
 	Name string
 	Size int64
 	Path string
+	Hash []byte
 }
 
 func main() {
+	var cPath string
+	flag.StringVar(&cPath, "p", "", "check path")
+	flag.Parse()
+
+	if len(cPath) == 0 {
+		fmt.Println("check path not found")
+		os.Exit(1)
+	}
+
 	allFile = make(map[int64][]*File)
 
 	src, err := os.Stat(cPath)
@@ -37,9 +52,16 @@ func main() {
 		}
 	}
 
-	for _, v := range allFile {
-		for _, f := range v {
-			fmt.Printf("Size: %d. Name: %s. Path: %s.\n", f.Size, f.Name, f.Path)
+	for _, files := range allFile {
+		if len(files) > 1 {
+			files = compareHash(files)
+			files = compareByte(files)
+			if len(files) > 1 {
+				fmt.Println("+++o")
+				for _, file := range files {
+					fmt.Println(file.Path)
+				}
+			}
 		}
 	}
 }
@@ -56,4 +78,76 @@ func walker(path string, fi os.FileInfo, err error) error {
 		allFile[file.Size] = files
 	}
 	return nil
+}
+
+func compareHash(files []*File) []*File {
+	sameHash := []*File{}
+
+	for _, f := range files {
+		hash, err := computeMd5(f.Path)
+		if err != nil {
+			panic(err)
+		}
+		f.Hash = hash
+	}
+
+	for _, i := range files {
+		for _, j := range files {
+			if !reflect.DeepEqual(i, j) {
+				if bytes.Compare(i.Hash, j.Hash) == 0 && !checkFilesContain(sameHash, j) && !checkFilesContain(sameHash, i) {
+					sameHash = append(sameHash, i)
+					sameHash = append(sameHash, j)
+				}
+			}
+		}
+	}
+	return sameHash
+}
+
+func checkFilesContain(files []*File, file *File) bool {
+	for _, f := range files {
+		if reflect.DeepEqual(f, file) {
+			return true
+		}
+	}
+	return false
+}
+
+func computeMd5(path string) ([]byte, error) {
+	var result []byte
+	file, err := os.Open(path)
+	if err != nil {
+		return result, err
+	}
+	digest := md5.New()
+	if _, err := io.Copy(digest, file); err != nil {
+		return result, err
+	}
+	return digest.Sum(result), nil
+}
+
+func compareByte(files []*File) []*File {
+	sameByte := []*File{}
+	for _, i := range files {
+		for _, j := range files {
+			if reflect.DeepEqual(i, j) {
+				break
+			}
+			if !checkFilesContain(sameByte, i) && !checkFilesContain(sameByte, j) {
+				f1, err := ioutil.ReadFile(i.Path)
+				if err != nil {
+					panic(err)
+				}
+				f2, err := ioutil.ReadFile(j.Path)
+				if err != nil {
+					panic(err)
+				}
+				if bytes.Compare(f1, f2) == 0 {
+					sameByte = append(sameByte, i)
+					sameByte = append(sameByte, j)
+				}
+			}
+		}
+	}
+	return sameByte
 }
